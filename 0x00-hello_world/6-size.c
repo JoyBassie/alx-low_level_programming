6a5e7d05231deccdbc465e8df47729281704458b
Joy <joybassie412@gmail.com>
2022-09-08 17:40:51 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,30 +1,14 @@
-
 #include <stdio.h>
- 
 /**
-
-  * main - Entry point
-  *
-  * Return: Always 0 (successfully)
-  
-  */
-
+* main - entry point
+* Return: 0
+*/
 int main(void)
 {
-
-          char charType;
-          int intType;
-          long int longIntType
-          long long int iLongIntType;
-          float floating;
-
-
-          /*Size of Variables*/
-          printtf("Size of a char: %zu byte(s)\n", sizeof(charType));
-          printf("Size of an int:%zu byte(s)\n",sizeof(intType));
-          printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-          printf("Size of a long long int: %zu byte(s)\n", sizeof(ilongIntType));
-          printf("Size of a float: %zu byte(s)\n", sizeof(floatInt));
-          return (0);
-
-
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
+return (0);
+}
