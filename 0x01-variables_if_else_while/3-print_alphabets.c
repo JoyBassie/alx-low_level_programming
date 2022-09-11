2413cdf44e32a8922f3de04d15a4ecb1df39e0c1
Joy <joybassie412@gmail.com>
2022-09-11 10:33:36 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,7 @@
-
-
-/* 
-* File: 3-print_alphabets.c
-* Author: Joy Bassey 
-*/
-
 #include <stdio.h>
-
 /**
 * main - Alphabets in lowercase
 *
-* Description : using the main function 
-* this program prints alphabet in lower case and in upper case
 * Return: 0
 */
 int main(void)
@@ -19,12 +9,13 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-putchar(ch);
+     putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 { 
-putchar(ch);
+     putchar(ch);
 }
 putchar('\n');
 return (0);
 }
+
