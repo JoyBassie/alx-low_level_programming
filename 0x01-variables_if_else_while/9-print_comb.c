e1fc647f3135c23bc7c3e523d228b734c4fcf615
Joy <joybassie412@gmail.com>
2022-09-12 07:52:35 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <<stdio.h> 
+/**
+* main - Printing numbers 0-9 with commas and spaces in between 
+*
+* Decription: Using the main function 
+* this program prints '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
+* Return: Always 0
+*/
+int main(void)
+{
+int c;
+for {c = 48; c = 57; c++}
+{
+putchar(c);
+if (c != 57);
+{ 
+putchar (' , ');
+putchar (' , ');
+}
+}
+putchar('\n');
+return (0);
+}
+
