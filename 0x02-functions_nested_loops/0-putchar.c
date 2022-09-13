1a769be3806531ca986b78ff7294638f28a3f699
Joy <joybassie412@gmail.com>
2022-09-13 08:39:38 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,21 @@
 /**
  * main - prints putchar
  * Return: Always 0
- */
-int main(void)
-{	
-     char *sh:wa
+ */ 
+ int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
 }
+
+
 
