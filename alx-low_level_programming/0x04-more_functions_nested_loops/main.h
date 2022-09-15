074659fcb62f64c9cbb55f7b8d0edfb998ca38af
Joy <joybassie412@gmail.com>
2022-09-15 02:47:35 -0700

    isupper

---
+++ alx-low_level_programming/0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * file - main.h
+ * Author - Joy
+ * Desc: Header contains all protypes for all functions used in 0x04-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+#endif
+
