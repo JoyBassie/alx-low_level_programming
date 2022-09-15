3bafbd08ea139ffbbdea841625ba534ddba1bc2c
Joy <joybassie412@gmail.com>
2022-09-15 12:32:38 -0700

    isupper

--- alx-low_level_programming/0x04-more_functions_nested_loops/main.h
+++ alx-low_level_programming/0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- * file - main.h
- * Author - Joy
- * Desc: Header contains all protypes for all functions used in 0x04-more_functions_nested_loops directory
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
  */
 
-int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void print_more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-#endif
+#endif /* MAIN_H */
 
