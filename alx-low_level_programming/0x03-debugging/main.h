953f10ab992850045e285000cb1b611b18711b10
Joy <joybassie412@gmail.com>
2022-09-16 03:42:28 -0700

    0-main.c

--- alx-low_level_programming/0x03-debugging/main.h
+++ alx-low_level_programming/0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-void poaitive_or_negative(int i);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
