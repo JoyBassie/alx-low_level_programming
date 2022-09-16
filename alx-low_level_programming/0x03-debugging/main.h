dfaf5110bb689eb094b93c9b119c9bc6ac9c1180
Joy <joybassie412@gmail.com>
2022-09-16 06:22:52 -0700

    0-main.c

--- alx-low_level_programming/0x03-debugging/main.h
+++ alx-low_level_programming/0x03-debugging/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 #include <stdio.h>
-void positive_or_negative( );
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
 
-#endif /* MAIN_H */
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_daylight(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif // MAIN_H_INCLUDED
