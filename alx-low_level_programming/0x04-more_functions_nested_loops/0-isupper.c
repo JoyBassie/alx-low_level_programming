88c76cd73ce4f02a8de8c95bb6c58bb1229de8db
Joy <joybassie412@gmail.com>
2022-09-16 19:58:20 -0700

    isupper

--- alx-low_level_programming/0x04-more_functions_nested_loops/0-isupper.c
+++ alx-low_level_programming/0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,19 @@
-#include "main.h"
-/**
-* _isupper - checks for uppercase characters.
-* @c: The character to be checked
-* Return: 1 if character is uppercase, 0 otherwise.
-*/
-int _isupper(int c)
-{
-if ((c >= 'A') && (c <= 'Z'))
-{
+#include"main.h" 
+#include <stdio.h> 
+ 
+/** 
+* _isupper - function to check if 
+* character is uppercase 
+* 
+* @c: checks input of function 
+* 
+* Return: returns 1 if `c` is uppercase 
+* otherwise always 0 (Success) 
+*/ 
+ 
+int _isupper(int c) 
+{ 
+if (c >= 65 && c <= 90)
 return (1);
-}
 return (0);
-
 }
--- alx-low_level_programming/0x04-more_functions_nested_loops/main.h
+++ alx-low_level_programming/0x04-more_functions_nested_loops/main.h
@@ -1,23 +1,19 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
-*main - header file for prototypes
-*void protoypes (void)
-*int protoypes (int)
-*/
-
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
+#ifndef MAIN_H_INCLUDED 
+#define MAIN_H_INCLUDED 
+ 
+#include <stdio.h> 
+ 
+int _putchar(char c); 
+int _isupper(int c); 
+int _isdigit(int c); 
+int mul(int a, int b); 
+void print_numbers(void); 
+void print_most_numbers(void); 
+void more_numbers(void); 
+void print_line(int n); 
+void print_diagonal(int n); 
+void print_square(int size); 
+void print_triangle(int size); 
 void print_number(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
 
-#endif /* MAIN_H */
+#endif
